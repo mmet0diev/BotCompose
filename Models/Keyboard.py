a98518b98f375da25fab94fe1de28e95c6401be0
mmet0diev <max.metodiev.1999@gmail.com>
2023-04-25 12:28:21 +0100

    .

--- Models/Keyboard.py
+++ Models/Keyboard.py
@@ -94,5 +94,5 @@ class Keyboard:
 
     # toString of KB
     def __str__(self) -> str:
-         return f"Component: {self.comp_name}\nAdded hotkeys:{self.hotkeys}\n"
+         return f"Component: {self.comp_name}\nAdded hotkeys:{self.hotkeys}"
     
