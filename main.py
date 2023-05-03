c1b400ef50c459ee9c9d327eb49ee0440a6898e6
mmet0diev <max.metodiev.1999@gmail.com>
2023-05-03 19:37:19 +0100

    added a gui with tkinter for the program

--- main.py
+++ main.py
@@ -11,94 +11,99 @@ bot = Bot()
 # Read commands from .txt file
 # C:\Users\maxur\Desktop\dsodaily1.txt
 def read_from_file(src_file: str):
-    print("Press 'esc' to stop command(s) execution.")
-    with open(src_file, "r") as f:
-        for line in f:
-            if bot.kb.check_key_pressed("esc"):
-                print("Execution stopped.")
-                break
-            if line != "":
-                cmd1 = line.strip().split(" ")
-                func = cmd1[0]
-                args = cmd1[1:]
-                match func:
-                    case "mv":
-                        x, y = map(float, args)
-                        bot.mv(x, y)
-                    case "clck":
-                        btn = args[0]
-                        bot.clck(btn)
-                    case "mvclck":
-                        x, y = map(float, args[:2])
-                        btn = args[2]
-                        bot.mvclck(x, y, btn)
-                    case "scroll":
-                        n = int(args[0])
-                        bot.scroll(n)
-                    case "press":
-                        key = args[0]
-                        bot.press(key)
-                    case "hld":
-                        comp = args[0]
-                        btn = args[1]
-                        if comp == 'mouse':
-                            bot.mouse_hld(btn=btn)
-                        elif comp == 'kb':
-                            bot.kb_hld(btn=btn)
-                    case "rel":
-                        comp = args[0]
-                        btn = args[1]
-                        if comp == 'mouse':
-                            bot.mouse_rel(btn=btn)
-                        elif comp == 'kb':
-                            bot.kb_rel(btn=btn)
-                    case "clckimg":
-                        if len(args) == 1:
-                            img_path = args[0]
-                            bot.clckimg(img_path)
-                        elif len(args) == 2:
-                            img_path = args[0]
-                            btn = args[1]
-                            bot.clckimg(img_path, btn=btn)
-                        elif len(args) == 3:
-                            img_path = args[0]
-                            btn = args[1]
-                            conf = args[2]
-                            bot.clckimg(img_path, btn=btn, conf=conf)
-                    case "drag":
-                        if(len(args) == 4):
-                            x1 = int(args[0])
-                            y1 = int(args[1])
-                            x2 = int(args[2])
-                            y2 = int(args[3])
-                            bot.drag(x1, y1, x2, y2)
-                        elif(len(args) == 5):
-                            x1 = int(args[0])
-                            y1 = int(args[1])
-                            x2 = int(args[2])
-                            y2 = int(args[3])
-                            dur = int(args[4])
-                            bot.drag(x1, y1, x2, y2, dur)
-                        else:
-                            print("Invalid arguments passed.")
-                    case "wrt":
-                        text = " ".join(args)
-                        bot.wrt(text)
-                    case "sleep":
-                        secs = float(args[0])
-                        bot.sleep(secs)
-                    case "shoot":
-                        bot.take_shot()
-                    case "repeat":
-                        reps = int(args[0])
-                        next_lines = int(args[1])
-                        bot.repeat_lines(f=f, reps=reps, n_lines=next_lines)
-                    case _:
-                        print(f"Invalid command(s)/syntax: {func}")
-            else:
-                print("Empty line")
-
+    # "Press 'esc' to stop command(s) execution.
+    if src_file != "":
+        with open(src_file, "r") as f:
+            for line in f:
+                try:
+                    if bot.kb.check_key_pressed("esc"):
+                        print("Execution stopped.")
+                        break
+                    if line != "":
+                        cmd1 = line.strip().split(" ")
+                        func = cmd1[0]
+                        args = cmd1[1:]
+                        match func:
+                            case "mv":
+                                x, y = map(float, args)
+                                bot.mv(x, y)
+                            case "clck":
+                                btn = args[0]
+                                bot.clck(btn)
+                            case "mvclck":
+                                x, y = map(float, args[:2])
+                                btn = args[2]
+                                bot.mvclck(x, y, btn)
+                            case "scroll":
+                                n = int(args[0])
+                                bot.scroll(n)
+                            case "press":
+                                key = args[0]
+                                bot.press(key)
+                            case "hld":
+                                comp = args[0]
+                                btn = args[1]
+                                if comp == 'mouse':
+                                    bot.mouse_hld(btn=btn)
+                                elif comp == 'kb':
+                                    bot.kb_hld(btn=btn)
+                            case "rel":
+                                comp = args[0]
+                                btn = args[1]
+                                if comp == 'mouse':
+                                    bot.mouse_rel(btn=btn)
+                                elif comp == 'kb':
+                                    bot.kb_rel(btn=btn)
+                            case "clckimg":
+                                if len(args) == 1:
+                                    img_path = args[0]
+                                    bot.clckimg(img_path)
+                                elif len(args) == 2:
+                                    img_path = args[0]
+                                    btn = args[1]
+                                    bot.clckimg(img_path, btn=btn)
+                                elif len(args) == 3:
+                                    img_path = args[0]
+                                    btn = args[1]
+                                    conf = args[2]
+                                    bot.clckimg(img_path, btn=btn, conf=conf)
+                            case "drag":
+                                if(len(args) == 4):
+                                    x1 = int(args[0])
+                                    y1 = int(args[1])
+                                    x2 = int(args[2])
+                                    y2 = int(args[3])
+                                    bot.drag(x1, y1, x2, y2)
+                                elif(len(args) == 5):
+                                    x1 = int(args[0])
+                                    y1 = int(args[1])
+                                    x2 = int(args[2])
+                                    y2 = int(args[3])
+                                    dur = int(args[4])
+                                    bot.drag(x1, y1, x2, y2, dur)
+                                else:
+                                    print("Invalid arguments passed.")
+                            case "wrt":
+                                text = " ".join(args)
+                                bot.wrt(text)
+                            case "sleep":
+                                secs = float(args[0])
+                                bot.sleep(secs)
+                            case "shoot":
+                                bot.take_shot()
+                            case "repeat":
+                                reps = int(args[0])
+                                next_lines = int(args[1])
+                                bot.repeat_lines(f=f, reps=reps, n_lines=next_lines)
+                            case _:
+                                print(f"Invalid command(s)/syntax: {func}")
+                    else:
+                        print("Empty line")
+                except Exception:
+                    print("Invalid command(s)/syntax.")
         f.close()
+    else:
+        print("Empty input field!")
 
 
 # Manually issue commands from the terminal(similar to REPL / interactive mode)
@@ -209,11 +214,13 @@ class AppUI():
         kb_play_btn = tk.Button(self.root, text="play kb", pady=5, command=self.bot.play_kb)
 
         file_read_label = tk.Label(self.root, text="Read from file ->")
-        file_read_btn = tk.Button(self.root, text="src file", pady=5)
+        file_input_field = tk.Entry(self.root)
+        file_read_btn = tk.Button(self.root, text="Run", pady=5)
+        file_read_btn.configure(command=lambda: read_from_file(file_input_field.get()))
 
         manual_label = tk.Label(self.root, text="Run commands manually ->")
-        input_field = tk.Entry(self.root)
-        man_run_btn = tk.Button(self.root, text="Run", command=lambda: manual_input(input_field.get()))
+        man_input_field = tk.Entry(self.root)
+        man_run_btn = tk.Button(self.root, text="Run", command=lambda: manual_input(man_input_field.get()))
 
         # Pack widgets
         bot_label.grid(row=0, column=0, columnspan=4) # span across all columns
@@ -222,9 +229,10 @@ class AppUI():
         mouse_play_btn.grid(row=1, column=2)
         kb_play_btn.grid(row=1, column=3)
         file_read_label.grid(row=2, column=0)
-        file_read_btn.grid(row=2, column=1)
+        file_input_field.grid(row=2, column=1)
+        file_read_btn.grid(row=2, column=2)
         manual_label.grid(row=3, column=0)
-        input_field.grid(row=3, column=1)
+        man_input_field.grid(row=3, column=1)
         man_run_btn.grid(row=3, column=2)
 
         # Center the bot_label widget
